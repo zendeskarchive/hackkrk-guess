80bb704ca2c169714952b42ea33c93f93565b13c
Tomek Paczkowski <tomek@hauru.eu>
2012-06-20 20:59:18 +0200

    Create user

--- guess.py
+++ guess.py
@@ -1,61 +1,113 @@
+import hashlib
+import os
+from os import environ
 from flask import Flask, request, jsonify
+from flask_heroku import Heroku
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from werkzeug.datastructures import MultiDict
+import wtforms
+from wtforms import validators
+
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+heroku = Heroku(app)
 
-riddle = {
-    'id': 1,
-    'question': 'What is your quest?',
-    'photo_url': 'http://f.cl.ly/items/1a3m2x1P3A0m1x3J031f/kitten.jpeg',
-    'attempted_by': 7,
-    'solved_by': 4,
-    'points': 2,
-    'solved': False,
-    'created_at': '2012-06-21T18:20:12Z',
-}
+if not 'DATABASE_URL' in environ:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-def a_riddle(id):
-    return dict(riddle, id=id)
 
-@app.route('/user', methods=['GET', 'POST'])
-def user():
+class UserForm(wtforms.Form):
+    username = wtforms.TextField(validators=[validators.required()])
+    password = wtforms.TextField(validators=[validators.required()])
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Unicode(100), unique=True)
+    password_hash = db.Column(db.Unicode(100))
+    token = db.Column(db.Unicode(100))
+
+    @property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password(self, value):
+        self.password_hash = hashlib.md5(value).hexdigest()
+
+    def is_password(self, value):
+        return self.password_hash == hashlib.md5(value).hexdigest()
+
+    def generate_token(self):
+        self.token = hashlib.md5(os.urandom(64)).hexdigest()
+        return self.token
+
+
+class Riddle(db.Model):
+    __tablename__ = 'riddles'
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.UnicodeText)
+    answer = db.Column(db.UnicodeText)
+    photo_url = db.Column(db.UnicodeText)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author = db.relationship(User, primaryjoin=(author_id==User.id))
+
+
+
+@app.route('/user', methods=['GET'])
+def validate_user():
     return jsonify({
-        'login': 'hackkrk',
-        'token': 'dead00coffee'
     })
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    try:
+        form = UserForm(MultiDict(request.json))
+        if form.validate():
+            user = User()
+            user.generate_token()
+            form.populate_obj(user)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({
+                'username': user.username,
+                'token': user.token,
+            })
+        else:
+            resp = jsonify(form.errors)
+            resp.status_code = 400
+            return resp
+    except IntegrityError:
+        resp = jsonify({
+            'username': "Username already taken",
+        })
+        resp.status_code = 400
+        return resp
 
 @app.route("/riddles", methods=["GET"])
 def riddles():
     return jsonify({
-        'riddles': [a_riddle(n+1) for n in range(5)],
-        'count': 5,
-        'page': 1,
-        'page_count': 1,
     })
 
 @app.route("/riddles", methods=["POST"])
 def post_riddle():
-    return jsonify(riddle)
+    return jsonify({
+
+    })
 
 @app.route("/riddles/<id>/answer", methods=["POST"])
 def answer_riddle(id):
     return jsonify({
-        "id": id,
-        "correct": True,
-        "points": 2,
-        "new_score": 10
     })
 
 @app.route("/leaderboard")
 def leaderboard():
     return jsonify({
-        'users': [
-            { "name": "isaac_newton", "score": "54" },
-            { "name": "carl_sagan", "score": "37" },
-        ],
-        'count': 2,
-        'page': 1,
-        'page_count': 1
     })
 
 if __name__ == '__main__':
+
     app.run(debug=True, port=8000)
