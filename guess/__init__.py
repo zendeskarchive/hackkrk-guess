7faa98d70024fb79b717e289198a60b56c331a86
Tomek Paczkowski <tomek@hauru.eu>
2012-06-22 01:29:33 +0200

    Sentry

--- guess/__init__.py
+++ guess/__init__.py
@@ -2,11 +2,14 @@ from os import environ
 from flask import Flask
 from flask_heroku import Heroku
 from flask.ext.sqlalchemy import SQLAlchemy
+from raven.contrib.flask import Sentry
 
 
 AUTH_TOKEN = 'X-Auth-Token'
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+sentry = Sentry(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///test.db')
+
